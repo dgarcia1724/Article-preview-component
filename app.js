fd8ce6865d1bbca57069f6d7d296c842b659ece1
dgarcia1724 <dgarcia1724@berkeley.edu>
2023-05-14 00:56:39 -0700

    step one

---
+++ app.js
@@ -0,0 +1,6 @@
+const shareBtn = document.querySelector(".shareBefore");
+const shareFooter = document.querySelector(".shareMobile");
+
+shareBtn.addEventListener("click", function () {
+  shareFooter.style.visibility = "visible";
+});
