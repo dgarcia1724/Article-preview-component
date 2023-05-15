80280520c85a286e943c3e3fd9090d21fba0ed47
dgarcia1724 <dgarcia1724@berkeley.edu>
2023-05-14 17:50:00 -0700

    fin

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const shareBtn = document.querySelector(".shareBefore");
+const shareBtnBefore = document.querySelector(".shareBefore");
+const shareBtnAfter = document.querySelector(".shareAfter");
 const shareFooter = document.querySelector(".shareMobile");
 
-shareBtn.addEventListener("click", function () {
+shareBtnBefore.addEventListener("click", function () {
   shareFooter.style.visibility = "visible";
+});
+
+shareBtnAfter.addEventListener("click", function () {
+  shareFooter.style.visibility = "hidden";
+});
+
+// Desktop
+const desktopBefore = document.querySelector(".desktopBefore");
+const popUp = document.querySelector(".popUp");
+
+desktopBefore.addEventListener("click", function () {
+  popUp.classList.toggle("show");
 });
